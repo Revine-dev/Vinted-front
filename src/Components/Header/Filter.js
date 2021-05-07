5e14d99db80c289d2b8dad5692403c2403d7f36f
Revine <rempowa@gmx.fr>
2021-05-07 14:51:40 +0200

    Filter

--- src/Components/Header/Filter.js
+++ src/Components/Header/Filter.js
@@ -1,78 +1,39 @@
-import { Range, getTrackBackground } from "react-range";
+import { Range } from "react-range";
 
 const Filter = ({ priceRange, setPriceRange }) => {
-  const priceMin = 0;
-  const priceMax = 500;
+  const priceMin = 0,
+    priceMax = 500;
 
   return (
     <Range
-      step={5}
+      step={0.1}
       min={priceMin}
       max={priceMax}
       values={priceRange}
-      onChange={(values) => setPriceRange(values)}
-      onFinalChange={(values) => {
-        setPriceRange(values);
-      }}
+      onChange={(values) => setPriceRange({ values })}
       renderTrack={({ props, children }) => (
-        <div
-          style={{
-            ...props.style,
-            height: "36px",
-            display: "flex",
-            width: "50%",
-          }}
-        >
-          <div
-            ref={props.ref}
-            style={{
-              height: "5px",
-              width: "100%",
-              borderRadius: "4px",
-              background: getTrackBackground({
-                values: priceRange,
-                colors: ["#ccc", " #2cb1ba", "#ccc"],
-                min: priceMin,
-                max: priceMax,
-              }),
-              alignSelf: "center",
-            }}
-          >
-            {children}
-          </div>
-        </div>
-      )}
-      renderThumb={({ index, props, isDragged }) => (
         <div
           {...props}
           style={{
             ...props.style,
-            height: "15px",
-            width: "15px",
-            borderRadius: "50%",
-            border: isDragged ? "" : "1px solid white",
-            backgroundColor: "#2cb1ba",
-            outline: "none",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            height: "6px",
+            width: "100%",
+            backgroundColor: "#ccc",
           }}
         >
-          <div
-            style={{
-              position: "absolute",
-              top: "-28px",
-              color: "#fff",
-              fontSize: "12px",
-              fontFamily: "Maison Neue",
-              padding: "4px",
-              borderRadius: "4px",
-              backgroundColor: "#2cb1ba",
-            }}
-          >
-            {priceRange[index]}€
-          </div>
+          {children}
         </div>
+      )}
+      renderThumb={({ props }) => (
+        <div
+          {...props}
+          style={{
+            ...props.style,
+            height: "42px",
+            width: "42px",
+            backgroundColor: "#999",
+          }}
+        />
       )}
     />
   );
